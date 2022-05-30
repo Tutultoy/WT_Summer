58c39afe20de475545084a2a839dc1198c1f6843
Saumitra Biswas <106428835+Tutultoy@users.noreply.github.com>
2022-05-30 23:35:45 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,88 @@
+<html>
+    <head>
+        <title>Registration</title>
+</head>
+<body>
+<h1>Registration Form<hr></h1>
+<form >
+<table>
+    <tr>
+        <td>
+            First Name:
+        </td>
+        <td>
+            <input type="text" name="fname">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Last Name:
+        </td>
+        <td>
+            <input type="text" name="fname">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Age:
+        </td>
+        <td>
+            <input type="text" name="fname">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Designation:
+        </td>
+        <td>
+            <input type="radio" name="designation" value="Junior Programmer">Junior Programmer
+            <input type="radio" name="designation" value="Senior Programmer">Senior Programmer
+            <input type="radio" name="designation" value="Project Lead">Project Lead
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Preferred language
+        </td>
+        <td>
+        <input type="checkbox" name="preferred language" value="Java">Java
+        <input type="checkbox" name="preferred language" value="PHP">PHP
+        <input type="checkbox" name="preferred language" value="C++">C++
+        </td>
+    </tr>
+    <tr>
+        <td>
+            E-mail:
+        </td>
+        <td>
+            <input type="text" name="fname">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Password:
+        </td>
+        <td>
+            <input type="text" name="fname">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Please choose a file
+        </td>
+        <td>
+        <input type="file" name="myfile">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="Submit" >   <input type="Submit" value="Reset">
+        </td>
+       
+           
+       
+    </tr>
+</table>    
+</form>
+</body>
+</html>
